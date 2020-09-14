3d2785701764f97a807e11f44499e4ab175753f8
Bui Song Toan <toanbs@giaohangtietkiem.vn>
2020-09-14 17:04:30 +0700

    Update binlog2sql.py

--- binlog2sql/binlog2sql.py
+++ binlog2sql/binlog2sql.py
@@ -103,8 +103,8 @@ class Binlog2sql(object):
                     # else:
                     #     raise ValueError('unknown binlog file or position')
                 if isinstance(binlog_event, GtidEvent):
-                    time = datetime.datetime.fromtimestamp(binlog_event.timestamp).isoformat()
-                    gtid = "%s ============================================> gtid next: %s" % (time, binlog_event.gtid)
+                    _time = datetime.datetime.fromtimestamp(binlog_event.timestamp).isoformat()
+                    gtid = "%s ============================================> gtid next: %s" % (_time, binlog_event.gtid)
                     if self.sql2file:
                         sql2file_path = self.sql2file + ".{}.log".format(current_time)
                         save_sql2file(sql2file_path, gtid)
