de419a85f953342135356788b3f0c30c3cf51b84
Bui Song Toan <toanbs@giaohangtietkiem.vn>
2020-09-14 16:43:16 +0700

    Update binlog2sql.py

--- binlog2sql/binlog2sql.py
+++ binlog2sql/binlog2sql.py
@@ -104,7 +104,7 @@ class Binlog2sql(object):
                     # else:
                     #     raise ValueError('unknown binlog file or position')
                 if isinstance(binlog_event, GtidEvent):
-                    print("============================================> %s" % binlog_event.gtid)
+                    print("============================================> gtid next: %s" % binlog_event.gtid)
                     # binlog_event.dump()
                     # dumper.dump(binlog_event.packet.gtid)
                     
@@ -124,10 +124,10 @@ class Binlog2sql(object):
                         if self.flashback:
                             f_tmp.write(sql + '\n')
                         else:
-                            print(sql)
-                            # if self.sql2file:
-                            #     sql2file_path = self.sql2file + ".{}.log".format(current_time)
-                            #     save_sql2file(sql2file_path, sql)
+                            # print(sql)
+                            if self.sql2file:
+                                sql2file_path = self.sql2file + ".{}.log".format(current_time)
+                                save_sql2file(sql2file_path, sql)
 
                 if not (isinstance(binlog_event, RotateEvent) or isinstance(binlog_event, FormatDescriptionEvent)):
                     last_pos = binlog_event.packet.log_pos
